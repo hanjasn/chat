e6bd7427462ad7843ace5c6a1e547439bdb1472a
hanjasn <hanjasn@gmail.com>
2021-09-09 19:28:12 -0700

    Remove react-emoji from message text

--- client/src/components/Chat/Messages/Message/Message.js
+++ client/src/components/Chat/Messages/Message/Message.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { emojify } from 'react-emoji';
 import './Message.css';
 
 const Message = ({ message: { username, text }, currentUsername }) => {
   return username === 'admin' ? (
     <div className="message-container d-flex justify-content-center">
-      <div className="message-box margin-no-name bg-dark text-light">{emojify(text)}</div>
+      <div className="message-box margin-no-name bg-dark text-light">{text}</div>
     </div>
   ) : username === currentUsername ? (
     <div className="message-container d-flex justify-content-end">
       <div className="message-box margin-no-name bg-primary text-light">
-        {emojify(text)}
+        {text}
       </div>
     </div>
   ) : (
     <div className="message-container d-flex justify-content-start">
       <div className="message-name">{username}</div>
-      <div className="message-box bg-light text-dark">{emojify(text)}</div>
+      <div className="message-box bg-light text-dark">{text}</div>
     </div>
   );
 };
